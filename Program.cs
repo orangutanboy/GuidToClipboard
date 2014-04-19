c0f02ce9813792e1d4dfb441e4af30e8c49bc480
M_J_O_N_E_S <M_J_O_N_E_S@hotmail.com>
2014-04-19 10:25:21 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace GuidToClipboard
+{
+    class Program
+    {
+        [System.STAThread]
+        static void Main(string[] args)
+        {
+            var format = "";
+
+            if (args.Length > 0)
+            {
+                format = args[0];
+            }
+
+            if (format.Contains("?"))
+            {
+                new AboutBox().ShowDialog();
+                return;
+            }
+
+            if (format.Length > 1)
+            {
+                format = "";
+            }
+
+            if (!"NDBPX".Contains(format))
+            {
+                format = "";
+            }
+
+            if (format == "")
+            {
+                try
+                {
+                    format = (string)(new AppSettingsReader().GetValue("Format", typeof(string)));
+                }
+                catch (InvalidOperationException)
+                { 
+                }
+            }
+
+            Clipboard.SetText(System.Guid.NewGuid().ToString(format));
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GuidToClipboard")]
+[assembly: AssemblyDescription("Generates a new GUID and copies it to the clipboard.\r\n\r\nIt accepts one command line argument which should be a single character; one of N, D, P, B or X. This determines the format of the GUID. If no argument is supplied, the format is taken from the config app setting \"Format\"\r\n\r\nFor more information about GUID string formats, see http://msdn.microsoft.com/en-us/library/97af8hh4(v=vs.110).aspx")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("M_J_O_N_E_S")]
+[assembly: AssemblyProduct("GuidToClipboard")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisibleAttribute(false)]
